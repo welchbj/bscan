7dfc7f78f9389c9e58219f8a7e91f320faa15f49
Brian Welch <welch18@vt.edu>
2018-10-20 16:03:35 -0400

    Remove excessive logging from directory skeleton setup

--- bscan/dir_gen.py
+++ bscan/dir_gen.py
@@ -12,9 +12,7 @@ from bscan.dir_structure import (
 from bscan.errors import BscanForceSkipTarget
 from bscan.io_console import (
     print_i_d1,
-    print_i_d2,
-    print_i_d3,
-    print_w_d1)
+    print_w_d2)
 from bscan.io_files import (
     create_dir,
     path_exists,
@@ -30,7 +28,7 @@ def create_dir_skeleton(target: str) -> None:
         target: The singular target of the scan.
 
     """
-    print_i_d1('Beginning creation of directory structure for target ', target)
+    print_i_d1(target, ': beginning creation of directory structure')
 
     base_dir = get_base_dir(target)
     if path_exists(base_dir):
@@ -39,34 +37,26 @@ def create_dir_skeleton(target: str) -> None:
                 'Base directory ' + base_dir + ' already exists, use '
                 '`--hard` option to force overwrite')
 
-        print_w_d1('Removing existing base directory ', base_dir)
+        print_w_d2(target, ': removing existing base directory ', base_dir)
         remove_dir(base_dir)
 
-    print_i_d1('Creating base directory at ', base_dir)
     create_dir(base_dir)
     notes_file = get_notes_txt_file(target)
-    print_i_d2('Creating notes.txt file at ', notes_file)
     touch_file(notes_file)
     recommendations_file = get_recommendations_txt_file(target)
-    print_i_d2('Creating recommendations.txt file at ', recommendations_file)
     touch_file(recommendations_file)
 
     loot_dir = get_loot_dir(target)
-    print_i_d2('Creating loot directory at ', loot_dir)
     create_dir(loot_dir)
     proof_file = get_proof_txt_file(target)
-    print_i_d3('Creating proof.txt file at ', proof_file)
     touch_file(proof_file)
     local_file = get_local_txt_file(target)
-    print_i_d3('Creating local.txt file at ', local_file)
     touch_file(local_file)
 
     services_dir = get_services_dir(target)
-    print_i_d2('Creating services directory at ', services_dir)
     create_dir(services_dir)
 
     sploits_dir = get_sploits_dir(target)
-    print_i_d2('Creating sploits directory at ', sploits_dir)
     create_dir(sploits_dir)
 
-    print_i_d1('Successfully completed directory skeleton setup')
+    print_i_d1(target, ': successfully completed directory skeleton setup')
